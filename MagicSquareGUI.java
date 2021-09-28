04279d20a83307f42b22ae320aafdef776617608
braneeth <43222404+braneeth13@users.noreply.github.com>
2021-09-28 12:34:34 -0400

    Update MagicSquareGUI.java

--- MagicSquareGUI.java
+++ MagicSquareGUI.java
@@ -1,4 +1,5 @@
 import java.lang.reflect.Array;
+import java.util.Arrays;
 
 import javax.swing.*;
 import BreezySwing.*;
@@ -7,13 +8,15 @@ import BreezySwing.*;
 public class MagicSquareGUI extends GBFrame{
 	
 	 	
-	    IntegerField sizeField = addIntegerField (0,1,2,1,1);
+	    //IntegerField sizeField = addIntegerField (0,1,2,1,1);
 	   
 	   
-	    JButton inputSize = addButton ("Input Size",4,1,2,1);
+	   
+	    JButton inputValues = addButton ("Validate", 4 ,1, 2, 1 );
 	   
 	    GBPanel matrix = addPanel(3,2,1,1);
 	    JLabel textLabel      = addLabel ("Size", 1,1,1,1);
+	    JMenuItem item2 = addMenuItem ("Dimensions", "2 x 2");
 	    JMenuItem item3 = addMenuItem ("Dimensions", "3 x 3");
 	    JMenuItem item4 = addMenuItem ("Dimensions", "4 x 4");
 	    JMenuItem item5 = addMenuItem ("Dimensions", "5 x 5");
@@ -27,13 +30,20 @@ public class MagicSquareGUI extends GBFrame{
 	    int [][] intGrid = new int [8][8];
 	     
 	    public void buttonClicked(JButton buttonObj){ //button click "sensor"
-	        gridPanel.removeAll();
+	        if (buttonObj == inputValues) {
+	        	int [][] values = getValues(grid);
+	        	System.out.println(Arrays.deepToString(values));
+	        	MagicSquare square = new MagicSquare(values);
+	        }
+	    }
+	    
+	    public void generateMagicSquare(int size) {
+	    	gridPanel.removeAll();
 	        for (int x = 0; x < size; x++) {
 	            for (int y = 0; y < size; y++) {
-	                grid[x-1][y-1] = gridPanel.addIntegerField(0, x + 1, y + 1, 1, 1);
+	                grid[x][y] = gridPanel.addIntegerField(0, x + 1, y + 1, 1, 1);
 	            }
 	        }
-	        
 	        
 	        
 	        //refresh panel
@@ -43,24 +53,34 @@ public class MagicSquareGUI extends GBFrame{
 	    
 	    
 	    public void menuItemSelected(JMenuItem menuItem){
-	    	
-	        if (menuItem == item3) {
-	            size = 3;
+	    	if (menuItem == item2) {
+	        	size = 2;
+	    		generateMagicSquare(2);
+	        }
+	        
+	    	else if (menuItem == item3) {
+	        	size=3;
+	    		generateMagicSquare(3);
 	        }
 	        else if (menuItem == item4) {
-	            size = 4;
+	        	size = 4;
+	        	generateMagicSquare(4);
 	        }
 	        else if (menuItem == item5) {
-	            size = 5;
+	        	size =5;
+	        	generateMagicSquare(5);
 	    	}
 	        else if (menuItem == item6) {
-	            size = 6;
+	        	size = 6;
+	        	generateMagicSquare(6);
 	        }
 	        else if (menuItem == item7) {
-	            size = 7;
+	        	size = 7;
+	        	generateMagicSquare(7);
 	        }
 	        else if (menuItem == item8) {
 	        	size = 8;
+	        	generateMagicSquare(8);
 	        	
 	        }
 	        
@@ -69,6 +89,21 @@ public class MagicSquareGUI extends GBFrame{
 	        
 	        
 	        
+	    }
+	    
+	    public int [][] getValues (IntegerField[][] fields) {
+	    	if (fields == null) {
+	    		return null;
+	    	} 
+	    	System.out.println(size);
+	    	int [][] values = new int[size][size];
+	    	for (int i = 1;i<=size;i++) {
+	    		
+	    		for (int j=1; j<=size; j++) {
+	    			values [i-1][j-1] = fields[i-1][j-1].getNumber();
+	    		}
+	    	}
+	    	return values;
 	    }
 	   
 
