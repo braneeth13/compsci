a2f1b899fb4886b5cb289fc3d70fbfb66856bc53
braneeth <43222404+braneeth13@users.noreply.github.com>
2021-10-15 10:34:10 -0400

    Create Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+public class Calculator {
+	private Employee[] data;
+	
+	public Calculator (Employee[] data) {
+		this.data = data;
+	}
+	
+	public String lowest() {
+		
+		
+		
+		return "";
+	}
+}
