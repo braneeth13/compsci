1e3c8e59df75f80354c6b003332701efc480b406
braneeth <43222404+braneeth13@users.noreply.github.com>
2021-10-27 13:38:37 -0400

    Update Sequence.java

--- Sequence.java
+++ Sequence.java
@@ -1,4 +1,22 @@
-
 public class Sequence {
-
+	public int [][]values;
+	
+	public Sequence (int[] array) {
+		array = array;
+	}
+	
+	
+	
+	public String getLongest () {
+		String output = "";
+		
+		int [][] array = new int [10][10];
+		for (int i=0; i<values.length; i++) {
+			if (values[i] ==values [i+1]) {
+				output = output + " ";
+			}
+			
+		}
+		return "";
+	}
 }
