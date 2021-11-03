9fc23d0a9d5c5a97aecda462addbc44480c708d0
braneeth <43222404+braneeth13@users.noreply.github.com>
2021-11-03 00:19:19 -0400

    Update SequenceGUI.java

--- SequenceGUI.java
+++ SequenceGUI.java
@@ -12,21 +12,30 @@ public class SequenceGUI extends GBFrame{
 	   
 	   
 	    JLabel field = addLabel("Enter Values Here", 1, 1, 1, 1);
-	    IntegerField intField = addIntegerField(0, 1, 3, 3, 3);
+	    IntegerField intField = addIntegerField(0, 1, 3, 1, 3);
+	    
+	    //buttons to input value, view the longest sequence, and reset
 	    JButton inputValues = addButton ("Input Value", 2 ,1, 3, 3 );
+	    JButton view = addButton("View Longest Sequence ", 5, 1, 3, 3);
+	    JButton reset = addButton("Reset Sequence", 7, 1, 3, 3);
+	    
 	    JTextArea output = addTextArea("", 4, 4, 2, 2);
 	    int [] array = new int [25];
 	    int count = 0;
 	    
+	    Sequence sequence = new Sequence();
+	    
 	    public void buttonClicked(JButton buttonObj){ //button click "sensor"
 	    	
 	    	if (buttonObj == inputValues) {
+	    		
+	    		
 	    		if(errorCheck()==true) {
 	    			if (count <25) {
 	    				array[count] = intField.getNumber();
 	    				output.append(intField.getNumber() + " ");
 	    				count++;
-	    				System.out.println(count);
+	    				//System.out.println(count);
 	    			}
 	    			if (count == 25) {
 	    				messageBox("You have reached the maximum number of inputs");
@@ -34,6 +43,23 @@ public class SequenceGUI extends GBFrame{
 	    		}else if (errorCheck() == false) {
 	    			messageBox("Invalid Input, please try again");
 	    		}
+	    	}
+	    	
+	    	if (buttonObj == view) {
+	    		messageBox("Length of longest sequence: " + sequence.getSequenceLength(array, count) + "The Longest Sequence(s) : " + sequence.getLongestSequence(array) );
+	    	}
+	    	
+	    	if (buttonObj == reset) {
+	    		for (int i = 0; i<25; i++) {
+	    			array[i] = 0;
+	    			count = 0;
+	    			
+	    		}
+	    		sequence.setLongest(0);
+	    		sequence.setCurrent(1);
+	    		sequence.setRepeat(1);
+	    		sequence.setRepeatIndex(new int [12]);
+	    		output.setText("");
 	    	}
 	    }
 	    
