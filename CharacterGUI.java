e26ed888a788516332b14b1569a3d919f031438d
braneeth <43222404+braneeth13@users.noreply.github.com>
2021-11-10 12:44:05 -0500

    Update CharacterGUI.java

--- CharacterGUI.java
+++ CharacterGUI.java
@@ -13,28 +13,42 @@ public class CharacterGUI extends GBFrame{
 	   
 	    JLabel label = addLabel("Enter the string here", 1, 1, 1, 1);
 	    JTextField field = addTextField ("",1 , 2, 4, 4);
+	    JTextArea output = addTextArea("", 1, 4, 10, 10);
 	    
-	    //buttons to input value, view the longest sequence, reset, and exit program
-	    JButton input = addButton ("Input", 1 ,2, 1, 1 );
+	    //buttons to input , view characteristics, reset, and exit program
+	    JButton input = addButton ("Input", 2,3, 1, 1 );
+	    JButton reset = addButton("Reset", 4, 3, 1, 1);
 	    
 	    String phrase = "";
 	 
 	    public void buttonClicked(JButton buttonObj){ //button click "sensor"
 	    	if (buttonObj == input) {
 	    		phrase = field.getText();
+	    		
+	    		Character character = new Character(phrase);
+	    		output.append("The number of characters is " + character.countCharacters());
+	    		output.append("\n");
+	    		output.append("The number of words is " + character.words());
+	    		output.append("\n");
+	    		
+	    		
+	    	
+	    		
+	    		
 	    	}
+	    	if (buttonObj == reset) {
+	    		output.setText("");
+	    		field.setText("");
+	    	}
+	    	
 	    }
 	    
 	    public boolean errorCheck() {
 	    	
-	    	if (2==2){
-	    		return true;
-	    	}
-	    	else {
-	    		return false;
-	    	}
-	    
+	    	return true;
 	    }
+	    
+	    
 	    public static void main(String[] args){
 	    	
 	        JFrame frm = new CharacterGUI();
@@ -42,4 +56,5 @@ public class CharacterGUI extends GBFrame{
 	        frm.setSize (900, 200);
 	        frm.setVisible (true);
 	   }
+	    
 }
