66681e6c79edffc01a528d093e8409857caac91a
braneeth <43222404+braneeth13@users.noreply.github.com>
2021-11-15 00:51:40 -0500

    Update CharacterGUI.java

--- CharacterGUI.java
+++ CharacterGUI.java
@@ -18,28 +18,36 @@ public class CharacterGUI extends GBFrame{
 	    //buttons to input , view characteristics, reset, and exit program
 	    JButton input = addButton ("Input", 2,3, 1, 1 );
 	    JButton reset = addButton("Reset", 4, 3, 1, 1);
-	    
+	    JButton exit = addButton("Exit", 6, 3, 1, 1);
+	   
 	    String phrase = "";
 	 
 	    public void buttonClicked(JButton buttonObj){ //button click "sensor"
 	    	if (buttonObj == input) {
 	    		phrase = field.getText();
 	    		
-	    		Character character = new Character(phrase);
-	    		output.append("The number of characters is " + character.countCharacters());
+	    		Sentence sentence = new Sentence(phrase);
+	    		output.append("The number of characters is " + sentence.countCharacters());
 	    		output.append("\n");
-	    		output.append("The number of words is " + character.words());
+	    		sentence.split();
+	    		output.append("The number of words is " + sentence.words());
 	    		output.append("\n");
-	    		
-	    		
-	    	
-	    		
-	    		
+	    		output.append("\n");
+	    		output.append("The frequencies are: " + "\n" + sentence.getFrequency());	    
+	    		output.append("\n");
+	    		output.append("\n");
+	    		output.append("In alphabetical order (with numbers first), the words are:" + "\n" + sentence.alphabeticalOrder());
+	    
 	    	}
 	    	if (buttonObj == reset) {
 	    		output.setText("");
 	    		field.setText("");
 	    	}
+	    	
+	    	if(buttonObj == exit) {
+	    		System.exit(0);
+	    	}
+	    	
 	    	
 	    }
 	    
@@ -52,7 +60,7 @@ public class CharacterGUI extends GBFrame{
 	    public static void main(String[] args){
 	    	
 	        JFrame frm = new CharacterGUI();
-	        frm.setTitle ("cHARACTER");
+	        frm.setTitle ("Character Program");
 	        frm.setSize (900, 200);
 	        frm.setVisible (true);
 	   }
